036c2840d82db3aa57abbff7d59db9f627b3a49e
jclum <joshclum@gmail.com>
2012-10-24 12:28:44 -0400

    Clum's initial commit

---
+++ sors/src/com/csnyder/model/Contact.java
@@ -0,0 +1,69 @@
+package com.csnyder.model;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Parent;
+
+@Entity
+public class Contact {
+	
+	@Id
+	public Long id;
+	
+    private String name;
+	
+    private String email;
+	
+    private Date date;
+    
+    @Parent
+    Key<addressBook> Book;
+    
+    public Long getId() {
+    	return id;
+    }
+    
+    public Key<addressBook> getBook() {
+		return Book;
+	}
+
+	public void setBook(Key<addressBook> book) {
+		Book = book;
+	}
+
+	public void setId(Long id) {
+    	this.id = id;
+    }
+    
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Contact() {
+		super();
+	}	
+}
